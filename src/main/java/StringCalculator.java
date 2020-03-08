ab179f5870753b8c0d5e3724b249853250671293
pinkninjajess <jessica.valenti1@gmail.com>
2020-03-08 19:41:05 -0400

    Updated to use stream

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,15 +1,19 @@
-import static java.lang.Integer.parseInt;
+import java.util.Arrays;
 
 public class StringCalculator {
+
+    // "//;1,2;3\n4" -> 10
+    // "//.1,2.3\n4" -> 10       // double slash registers new separator
+    // "//[;;;]1,2;;;3\n4" -> 10 // new separator between the square brackets
     public int add(String numbers) {
         if (numbers.length() == 0) {
             return 0;
         }
-        int sum = 0;
-        String[] numbersArray = numbers.split("[,\n]");
-        for (String s : numbersArray) {
-            sum += parseInt(s);
-        }
-        return sum;
+
+        return Arrays.stream(numbers.split(
+                "[,\n]"))
+                .filter((s) -> s.matches("\\d+"))
+                .mapToInt(Integer::valueOf)
+                .reduce(0, Integer::sum);
     }
 }
