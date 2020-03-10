b5c50a2bfc2167082ae332db3ac2fc7b34968660
pinkninjajess <jessica.valenti1@gmail.com>
2020-03-10 18:14:24 -0400

    Refactored code to use many methods - separated from add function

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -9,27 +9,37 @@ public class StringCalculator {
             return 0;
         }
 
-        String customDelimiter = "";
-        String numbersWithoutDelimiterDefinition = numbers;
+        return Arrays.stream(removeDelimitersFrom(numbers).split(getDelimitersOf(numbers)))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
 
+    private String getDelimitersOf(String numbers) {
+        return ",|\\n" + getCustomDelimiter(numbers);
+    }
+
+    private String removeDelimitersFrom(String numbers) {
+        if (numbers.startsWith("//[")) {
+            String[] parts = numbers.split("//\\[(.*?)]", 2);
+            return parts[1];
+        }
+        if (numbers.startsWith("//")) {
+            return numbers.substring(3);
+        }
+        return numbers;
+    }
+
+    private String getCustomDelimiter(String numbers) {
         if (numbers.startsWith("//[")) {
             Pattern p = Pattern.compile("//\\[(.*?)]");
             Matcher m = p.matcher(numbers);
             if (m.find()) {
-                customDelimiter = "|" + "(" + Pattern.quote(m.group(1)) + ")";
+                return "|" + "(" + Pattern.quote(m.group(1)) + ")";
             }
-            String[] parts = numbers.split("//\\[(.*?)]", 2);
-            numbersWithoutDelimiterDefinition = parts[1];
         } else if (numbers.startsWith("//")) {
-            numbersWithoutDelimiterDefinition = numbers.substring(3);
-            customDelimiter = "|" + Pattern.quote(numbers.substring(2, 3));
+            return "|" + Pattern.quote(numbers.substring(2, 3));
         }
-
-        String regex = ",|\\n" + customDelimiter;
-
-        return Arrays.stream(numbersWithoutDelimiterDefinition.split(regex))
-                .mapToInt(Integer::parseInt)
-                .sum();
+        return "";
     }
 
 }
